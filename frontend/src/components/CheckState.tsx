ba5d3c367b9490d3eca8c880be6b151a1f0d8012
Jeffrey Cheung <jeffreycheung_@live.com>
2023-05-29 10:22:47 -0700

    update state to change when bets open

--- frontend/src/components/CheckState.tsx
+++ frontend/src/components/CheckState.tsx
@@ -1,21 +1,25 @@
 import { Heading, Spinner } from '@chakra-ui/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useContractRead } from 'wagmi';
 
 export default function CheckState() {
   const [lotteryState, setLotteryState] = useState();
   const [isloading, setIsLoading] = useState(true);
 
+  const { data } = useContractRead({ watch: true });
+
   useEffect(() => {
     axios.get('http://localhost:3000/state')
-    .then(response => {
+      .then(response => {
         setLotteryState(response.data);
         setIsLoading(false);
+        console.log('watching');
       })
       .catch(error => {
         console.error(error);
       });
-  });
+  }, [data]);
 
   { isloading && <Spinner /> }
 
@@ -23,3 +27,4 @@ export default function CheckState() {
     <Heading>{lotteryState}</Heading>
   )
 }
+
